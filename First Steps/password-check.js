d04db1db4e9327af6a3d486f8d658c8227134d62
Becoming-ageekagain <wasimonmail4@gmail.com>
2023-10-04 11:15:50 -0400

    Password-checker -variable dec

---
+++ First Steps/password-check.js
@@ -0,0 +1,7 @@
+let passInput =document.querySelector('#pass-input');
+let passStrength=document.getElementById('password-strength');
+let poor =document.querySelector('#poor');
+let weak =document.querySelector('#weak');
+let strong =document.getElementById('strong');
+let passwordInfo=document.querySelector('#password-info');
+
