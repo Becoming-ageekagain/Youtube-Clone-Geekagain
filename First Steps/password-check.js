21471d446cfefcb8f852ba087ec593f941f9e777
Becoming-ageekagain <wasimonmail4@gmail.com>
2023-10-04 11:46:49 -0400

    code in progress - assessing logic

--- First Steps/password-check.js
+++ First Steps/password-check.js
@@ -5,3 +5,17 @@ let weak =document.querySelector('#weak');
 let strong =document.getElementById('strong');
 let passwordInfo=document.querySelector('#password-info');
 
+let poorRegExp=/[a-z]/;
+let weakRegExp=/[A-Z]/;
+let strongRegExp=/($#@!)/;
+
+let poorPass= poorRegExp.test(passInput.value);
+let weakPass= weakRegExp.test(passInput.value);
+let strongPass= strongRegExp.test(passInput.value);
+
+
+
+if(passInput.value==='')
+{
+passwordInfo.innerHTML = `The passoword is weak`;
+}
